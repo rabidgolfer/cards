bcb0cdcafed488e46c0db0fd700c611097062923
Travis Bowlby <travisbowlby1@gmail.com>
2021-12-19 20:47:38 -0500

    Added tests

---
+++ deck_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52 but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	const n string = "_decktesting"
+
+	os.Remove(n)
+
+	deck := newDeck()
+	deck.saveToFile(n)
+
+	loadedDeck := newDeckFromFile(n)
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove(n)
+}
